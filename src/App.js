8fd34ff05af415d2897a7d69bde6acec94fde096
Craig Puxty <craig_puxty@talktalk.net>
2023-11-29 15:03:43 +0000

    Added App static assets.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
 export default function App() {
-  return <div className='steps'>App</div>;
+  const step = 1;
+
+  return (
+    <div className='steps'>
+      <div className='numbers'>
+        <div className='active'>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      </div>
+      <p className='message'>
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className='buttons'>
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
 }
