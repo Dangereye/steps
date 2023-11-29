182a16a65d9e28384e1ead8dc068abe28f5e8dba
Craig Puxty <craig_puxty@talktalk.net>
2023-11-29 16:01:37 +0000

    Added step state and onclick event handlers.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const messages = [
   'Learn React ⚛️',
   'Apply for jobs 💼',
@@ -5,21 +7,28 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    if (step > 1) setStep((s) => s - 1);
+  };
+  const handleNext = () => {
+    if (step < 3) setStep((s) => s + 1);
+  };
 
   return (
     <div className='steps'>
       <div className='numbers'>
         <div className='active'>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
       <p className='message'>
         Step {step}: {messages[step - 1]}
       </p>
       <div className='buttons'>
-        <button>Previous</button>
-        <button>Next</button>
+        <button onClick={handlePrevious}>Previous</button>
+        <button onClick={handleNext}>Next</button>
       </div>
     </div>
   );
